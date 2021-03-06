3177ed61f628f2c0406a6e5a46fba17ccf36f3ac
Akash Shetty <akashlives@gmail.com>
2021-03-06 13:13:56 -0500

    Started implementing Data Cleaning module

---
+++ DataCleaning.py
@@ -0,0 +1,18 @@
+"""Object Oriented Data Science for cleaning Data
+Author - Akash Shetty
+Contributors -
+Date - March 6, 2021
+"""
+
+# Load libraries
+import pandas as pd
+
+
+class DataCleaning:
+    def __init__(self, *args):
+        raise NotImplementedError
+
+    def EDA(self, *args):
+        raise NotImplementedError
+
+        "so on"
